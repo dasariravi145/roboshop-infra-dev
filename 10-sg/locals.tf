f5da4c93ba648d55f10c2ce4cd6ff6e265e1c572
unknown <dasariravid145@gmail.com>
2026-06-05 09:04:58 +0530

    terraform

--- 10-sg/locals.tf
+++ 10-sg/locals.tf
@@ -2,3 +2,4 @@ locals {
 
       vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
+
--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -3,6 +3,7 @@ module "sg"{
      source = "git::https://github.com/dasariravi145/terraform-aws-sg.git"
      project = "roboshop"
      environment = "dev"
-     sg_name = replace(var.sg_name[count.index], "_","-")
+     sg_name = replace(var.sg_names[count.index], "_","-")
+     vpc_id = local.vpc_id
      
 }
