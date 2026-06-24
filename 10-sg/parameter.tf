12f4a4dc21f0df8c31486d91acb924cb9d872870
unknown <dasariravid145@gmail.com>
2026-06-24 23:41:54 +0530

    terraform

--- 10-sg/parameter.tf
+++ 10-sg/parameter.tf
@@ -1,7 +1,6 @@
-resource "aws_ssm_parameter" "mongodb_sg_id" {
-
-      count = length(var.sg_name)
-      name = "/${var.project}/${var.environment}/mongodb_sg_id"
+resource "aws_ssm_parameter" "sg_id" {
+      count = length(var.sg_names)
+      name = "/${var.project}/${var.environment}/${var.sg_names[count.index]}_sg_id"
       type = "String"
-      value = module.sg.sg_id
+      value = module.sg[count.index].sg_id
 }
