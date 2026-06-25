b10b17e60731d783f71408781bd47db81298da28
unknown <dasariravid145@gmail.com>
2026-06-25 21:50:01 +0530

    terraform

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -4,7 +4,19 @@ resource "aws_instace" "bastion" {
        instance_type = "t3.micro"
        subnet_id = local.public_subnet_id
        vpc_security_group_ids= [local.bastion_sg_id]
+       iam_instance_profile = aws_aim_instance_profile.bastion.name
+       user_data = file(bastion.sh)
+       root_block_device {
 
+            volume_size = 50
+            volume_type = "gp3"
+            tags = merge(
+                {
+                    Name = "{var.project}-{var.environment}-bastion"
+                }, 
+                local.common_tags
+            )
+       }
     tags = merge(
         {
               Name = "{var.project}-{var.environment}-bastion"
@@ -12,3 +24,49 @@ resource "aws_instace" "bastion" {
         local.common_tags
     )
 }
+
+resource "aws_iam_role" "bastion" {
+
+       name = "RoboShopDevBastion"
+
+    assume_role_policy=jsonencode({
+
+          version = "2012-10-17"
+          Statement = [
+              {
+                   Action = "sts:AssumeRole"
+                   Effect = "Allow"
+                   Sid =""
+                   Principal = {
+                        Service = "ec2.amazonaws.com"
+                   }
+              }
+          ]
+    }
+    )
+
+   tags = merge(
+        {
+              Name = "{var.project}-{var.environment}-bastion"
+        }, 
+        local.common_tags
+  )
+
+}
+
+resource "aws_iam_role_policy_attachment" "bastion"{
+
+       role= aws_iam_role.bastion.name
+       policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+
+resource "aws_aim_instance_profile" "bastion" {
+
+      name = "${var.project}-${var.environment}-bastion"
+      role = aws_iam_role.bastion.name
+}
+
+
+
+
