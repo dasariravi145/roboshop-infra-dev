7d39ffb9bd1f9503f9ef7f397e269f7f354f4ed0
unknown <dasariravid145@gmail.com>
2026-08-15 22:51:57 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -31,3 +31,16 @@ resource "aws_lb_listener" "http" {
         }
     }
 }
+
+resource "aws_route53_record" "www" {
+
+      zone_id = var.zone_id
+      name = "*.backend-alb-${var.environment}.${var.domain_name}"
+      type = "A"
+
+      alias {
+          name = aws_lb.backend_alb.dns_name
+          zone_id = aws_route53_zone.backend_alb.zone_id
+          evaluate_target_health = true
+      }
+}
